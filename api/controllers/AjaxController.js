1c1336e42282a812f34e0c4028e1d462d98e0d25
Kate Burgett <katelyn.burgett@gmail.com>
2014-11-19 23:01:38 -0500

    Add apis/callbacks for WMATA bus and train data for RI Ave Station

--- api/controllers/AjaxController.js
+++ api/controllers/AjaxController.js
@@ -2,7 +2,7 @@ var request = require("request")
 
 module.exports = {
 	
-  index: function(req, res) {
+  train: function(req, res) {
     
     var callback = function(err, body, data) {
       res.json(data)
@@ -13,7 +13,50 @@ module.exports = {
       json: true
     }, callback)
     
-  }
+  },
   
+  bus: function(req, res) {
+    /*
+    var callback = function(err, body, data) {
+      res.json(data)
+    }
+    */
+
+    apiFlipper = [
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003275&api_key=qrc78gh5rrfhccxn3az498fa",
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003274&api_key=qrc78gh5rrfhccxn3az498fa",
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1001765&api_key=qrc78gh5rrfhccxn3az498fa",
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003386&api_key=qrc78gh5rrfhccxn3az498fa",
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1002958&api_key=qrc78gh5rrfhccxn3az498fa",
+      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1002959&api_key=qrc78gh5rrfhccxn3az498fa",
+    ]
+
+    /*
+
+    for (i = 0; i < apiFlipper.length; i++) {   
+      request({
+        url: apiFlipper[i],
+        json: true
+      }, callback)
+    }
+    */
+
+    var iterator = function(item, itemCallback) {
+      request({
+        url: item,
+        json: true
+      }, function(err, body, data) {
+        itemCallback(err, data)
+      })
+    }
+
+    var callback = function(err, results) {
+      res.json(results)
+    }
+
+    async.map(apiFlipper, iterator, callback)
+
+  }
+
 };
 
