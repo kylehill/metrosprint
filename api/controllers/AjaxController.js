06994c4d27b1b432539d95697336b3f3f6b5d766
Kyle Hill <kylehill@gmail.com>
2014-11-19 23:13:41 -0500

    Moves API keys to config/locals

--- api/controllers/AjaxController.js
+++ api/controllers/AjaxController.js
@@ -16,35 +16,26 @@ module.exports = {
   },
   
   bus: function(req, res) {
-    /*
-    var callback = function(err, body, data) {
-      res.json(data)
-    }
-    */
-
-    apiFlipper = [
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003275&api_key=qrc78gh5rrfhccxn3az498fa",
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003274&api_key=qrc78gh5rrfhccxn3az498fa",
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1001765&api_key=qrc78gh5rrfhccxn3az498fa",
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1003386&api_key=qrc78gh5rrfhccxn3az498fa",
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1002958&api_key=qrc78gh5rrfhccxn3az498fa",
-      "http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1002959&api_key=qrc78gh5rrfhccxn3az498fa",
+    var stops = [
+      "1003275",
+      "1003274",
+      "1001765",
+      "1003386",
+      "1002958",
+      "1002959",
     ]
 
-    /*
-
-    for (i = 0; i < apiFlipper.length; i++) {   
+    var iterator = function(stopId, itemCallback) {
+      var key = _.sample(sails.config.WmataApiKeys)
+      
       request({
-        url: apiFlipper[i],
-        json: true
-      }, callback)
-    }
-    */
-
-    var iterator = function(item, itemCallback) {
-      request({
-        url: item,
-        json: true
+        url: "http://api.wmata.com/NextBusService.svc/json/jPredictions",
+        json: true,
+        qs: {
+          "api_key": key,
+          "subscription-key": key,
+          "StopID": stopId
+        }
       }, function(err, body, data) {
         itemCallback(err, data)
       })
@@ -54,7 +45,7 @@ module.exports = {
       res.json(results)
     }
 
-    async.map(apiFlipper, iterator, callback)
+    async.map(stops, iterator, callback)
 
   }
 
